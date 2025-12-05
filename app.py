6c202d330a8792ccbec4d0e10a18ce88d49e9f37
Devon-th <devon.thai@csedge.org>
2025-12-04 20:32:18 -0800

    sound added - micah

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
-# import playsound
+
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+import pygame, time #new sound stuff to test
+
+
 
 ai_list =["aigirl.jpeg", "aiguy.jpeg","aiwoman,jpeg","aiwoman8.jpeg","aiguy11.jpeg","aiwoman12.jpeg"]
 
 real_list =["realguy.jpeg","almondreal.webp","realguy3.jpeg","reallady7.jpeg","realman9.jpeg","realguy10.jpeg"]
 
-# initialval = 0
-# scorevar = initialval
-# pagenum = 7
-#Keep checking when scorevar changes; thats when you know user got correct answer. Inside the loop, if changed == true; Play sound to notify the user got the correct answer. 
+pygame.mixer.init()
+
 global scorevar
-# def addscore(score):
-#     scorevar += score
-# playsound(the path) # add in the path for the file and then replace all lines that manually add it to play the sound 
+scorevar = 0
+
 
 @app.route("/")
 def index():
@@ -30,6 +33,12 @@ def aior2(answer):
     global scorevar
     if answer == "ai": #handles previous pages' logic/ Page 1 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "real":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("2.html")
 
 @app.route("/3/<answer>")
@@ -37,6 +46,12 @@ def aior3(answer):
     global scorevar
     if answer == "ai": #handles previous pages' logic/ Page 2 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "real":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("3.html")
 
 @app.route("/4/<answer>")
@@ -44,6 +59,12 @@ def aior4(answer):
     global scorevar
     if answer == "real": #handles previous pages' logic/ Page 3 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "ai":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("4.html")
 
 @app.route("/5/<answer>")
@@ -51,6 +72,12 @@ def aior5(answer):
     global scorevar
     if answer == "ai": #handles previous pages' logic/ Page 4 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "real":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("5.html")
 
 @app.route("/6/<answer>")
@@ -58,6 +85,12 @@ def aior6(answer):
     global scorevar
     if answer == "real": #handles previous pages' logic/ Page 5 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "ai":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("6.html")
 
 @app.route("/7/<answer>")
@@ -65,41 +98,77 @@ def aior7(answer):
     global scorevar
     if answer == "real": #handles previous pages' logic/ Page 6 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "ai":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("7.html")
 
 @app.route("/8/<answer>")
-def aior7(answer):
+def aior8(answer):
     global scorevar
-    if answer == "real": #handles previous pages' logic/ Page 6 answer
+    if answer == "real": #handles previous pages' logic/ Page 7 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "ai":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("8.html")
 
 @app.route("/9/<answer>")
 def aior9(answer):
     global scorevar
-    if answer == "real": #handles previous pages' logic/ Page 6 answer
+    if answer == "ai": #handles previous pages' logic/ Page 8 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "real":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("9.html")
 
 @app.route("/10/<answer>")
 def aior10(answer):
     global scorevar
-    if answer == "real": #handles previous pages' logic/ Page 6 answer
+    if answer == "real": #handles previous pages' logic/ Page 9 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "ai":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("10.html")
 
 @app.route("/11/<answer>")
 def aior11(answer):
     global scorevar
-    if answer == "real": #handles previous pages' logic/ Page 6 answer
+    if answer == "real": #handles previous pages' logic/ Page 10 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "ai":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("11.html")
 
 @app.route("/12/<answer>")
 def aior12(answer):
     global scorevar
-    if answer == "real": #handles previous pages' logic/ Page 6 answer
+    if answer == "ai": #handles previous pages' logic/ Page 11 answer
         scorevar = scorevar + 1
+        pygame.mixer.music.load('static/ding.mp3')
+        pygame.mixer.music.play()
+ 
+    elif answer == "real":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("12.html")
 
 @app.route("/score/<answer>")
@@ -107,6 +176,12 @@ def score(answer):
     global scorevar
     if answer == "ai":
             scorevar = scorevar + 1
+            pygame.mixer.music.load('static/ding.mp3')
+            pygame.mixer.music.play()
+ 
+    elif answer == "real":
+        pygame.mixer.music.load('static/buzzer.mp3')
+        pygame.mixer.music.play()
     return render_template("score.html",scorevar = scorevar) #find way to print finalscore on html page
 
 @app.route("/aboutmyproject")
